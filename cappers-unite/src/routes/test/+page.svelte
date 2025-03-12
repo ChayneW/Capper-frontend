17bd0419b5fd110b2fe895dadbcd4b98c8be33a9
Chayne White <chaynesinghwhite@gmail.com>
2025-03-12 17:02:57 -0400

    first frontend commit

---
+++ cappers-unite/src/routes/test/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let message = "Loading...";
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+    async function fetchMessage() {
+        console.log('calling from svelete backend:\n')
+        try {
+            //const res = await fetch("http://localhost:8080/api/hello");
+            const res = await fetch(`${BACKEND_URL}/hello`);
+            const data = await res.json();
+            message = data.message;
+        } catch {
+            message = "Failed to fetch data.";
+            console.error(error);
+        }
+    }
+
+    fetchMessage();
+</script>
+
+<h1>{message}</h1>
+<h1>calling from /test</h1>
